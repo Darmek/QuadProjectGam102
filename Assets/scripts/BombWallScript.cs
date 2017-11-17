53f4a3fe8eef07cc2dfb8c8df7d956694c5647a5
Darmek <adunne1@mail.pima.edu>
2017-11-17 15:37:50 -0700

    HUGE PUSH. this is the duo game that Tim and I created. making a new gitproject that way we have the original to fall back if the worst happens and we are forced 2 go back. Please use the branching that drew showed us in class. that way we can merge to the main when we are ready without screwing eachother up.

---
+++ Assets/scripts/BombWallScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombWallScript : MonoBehaviour {
+
+    public BoxCollider2D BombWall;
+    public bool HitByBomb;
+
+	// Use this for initialization
+	void Start () {
+
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (HitByBomb)
+        {
+            Destroy(BombWall.gameObject);
+            print("Destroying the wall!");
+        }
+        else if (HitByBomb == false)
+        {
+            print("I can't destroy the wall!");
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bomb")
+        {
+            print("A bomb is hitting the wall!");
+            HitByBomb = true;
+        }
+        else
+        {
+            HitByBomb = false;
+        }
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    private float currentMoveSpeed;
+    public float diagonalMoveModifier;
+
+    private string characterDirection;
+
+    private Animator anim;
+    private Rigidbody2D myRigidbody;
+
+    private bool playerMoving;
+    public Vector2 lastMove;
+
+    private static bool playerExists;
+
+    private bool attacking;
+    public float attackTime;
+    private float attackTimeCounter;
+
+    public string startPoint;
+
+    public int keyCount;
+    public int bombCount;
+
+    public GameObject setBomb;
+
+	// Use this for initialization
+	void Start () {
+
+        anim = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        if (!playerExists)
+        {
+            playerExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerMoving = false;
+
+        if (!attacking)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.05f)
+            {
+                // transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+                myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * currentMoveSpeed, myRigidbody.velocity.y);
+                playerMoving = true;
+                lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            }
+
+            if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.05f)
+            {
+                // transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * currentMoveSpeed);
+                playerMoving = true;
+                lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+            }
+
+            if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+            {
+                myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
+            }
+            if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.05f)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                attackTimeCounter = attackTime;
+                attacking = true;
+                myRigidbody.velocity = Vector2.zero;
+                anim.SetBool("Attack", true);
+            }
+
+            if (Mathf.Abs (Input.GetAxisRaw("Horizontal")) > 0.5f && Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
+                {
+                currentMoveSpeed = moveSpeed * diagonalMoveModifier;
+                }
+            else
+            {
+                currentMoveSpeed = moveSpeed;
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.E) && bombCount > 0)
+            {
+                GameObject.Instantiate(setBomb, transform.position + (transform.forward * 2), transform.rotation);
+                print("Setting down the bomb!");
+                bombCount -= 1;
+            }
+            else if(Input.GetKeyDown(KeyCode.E) && bombCount <= 0)
+            {
+                print("I'm out of bombs.");
+            }
+                        
+        }
+
+        if (attackTimeCounter > 0)
+        {
+            attackTimeCounter -= Time.deltaTime;
+        }
+
+        if(attackTimeCounter <= 0)
+        {
+            attacking = false;
+            anim.SetBool("Attack", false);
+        }
+
+            anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Key")
+        {
+            print("Picking up the key");
+            Destroy(collision.gameObject);
+            keyCount += 1;
+        }
+
+        else if (collision.gameObject.tag == "KeyDoor")
+        {
+            if(keyCount > 0)
+            {
+                print("Unlocking the door");
+                Destroy(collision.gameObject);
+                keyCount -= 1;
+            }
+
+            else
+            {
+                print("I can't unlock that right now.");
+            }
+        }
+
+        else if (collision.gameObject.tag == "staticBomb")
+        {
+            print("Picking up bombs");
+            Destroy(collision.gameObject);
+            bombCount = +1;
+        }
+    }
+
+
+}
